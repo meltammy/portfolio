68b46439e58ffb603f38a1f9d2a228866eebea5c
Melissa Tammy <mel.tammy2@gmail.com>
2020-09-24 18:51:30 -0300

    token curriculo

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -8,7 +8,7 @@ function AboutMe(props) {
       <TextAboutMe darkMode={props.darkMode} className='textAboutMe'>
         <h1> Melissa Tammy</h1>
         <p>Desenvolvedora Web Full Stack, estudante de Análise e Desenvolvimento de sistemas, ex-aluna de Ciências biológicas e fotógrafa por hobby.</p>
-        <CV darkMode={props.darkMode} href="https://firebasestorage.googleapis.com/v0/b/meltammy-fda67.appspot.com/o/Meltammy_curriculo.pdf?alt=media&token=6e8f4b88-721e-437c-8996-5640d68c0ead" download>CV em PDF</CV>
+        <CV darkMode={props.darkMode} href="https://firebasestorage.googleapis.com/v0/b/meltammy-fda67.appspot.com/o/Meltammy_curriculo.pdf?alt=media&token=7b42b379-f7b6-4585-9e11-be5d0bcecf59" download>CV em PDF</CV>
       </TextAboutMe>
     </Container>
   );
