a680f5d7334c2128c3f1423078a96d15fcc5725c
Melissa Tammy <mel.tammy2@gmail.com>
2020-09-24 18:29:07 -0300

    Updates

---
+++ precache-manifest.88d25de0eca50dc450cfda66063124e2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff3503ee05c2220ce7488faa588a4133",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "19cd2c58fc16bfc72094",
+    "url": "/portfolio/static/css/main.4cdbf78a.chunk.css"
+  },
+  {
+    "revision": "fc6ca0f46bea9113dba5",
+    "url": "/portfolio/static/js/2.ea3d5762.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.ea3d5762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19cd2c58fc16bfc72094",
+    "url": "/portfolio/static/js/main.9e454679.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "dd88fa0a8ce1f1fac9cbfe3cb08d3b84",
+    "url": "/portfolio/static/media/Meltammy_curriculo.dd88fa0a.pdf"
+  },
+  {
+    "revision": "62ac87e8c2181c3b3eb5ab6c86c759d9",
+    "url": "/portfolio/static/media/aboutme2.62ac87e8.png"
+  },
+  {
+    "revision": "b270d951aed6507dafaace9b391699d1",
+    "url": "/portfolio/static/media/back.b270d951.png"
+  },
+  {
+    "revision": "a83b1acd3bde569cda149da3663772fc",
+    "url": "/portfolio/static/media/front.a83b1acd.png"
+  },
+  {
+    "revision": "ee13e17d736cffd3235fc164da67bbb3",
+    "url": "/portfolio/static/media/ipad2.ee13e17d.png"
+  },
+  {
+    "revision": "da7c6d1a74086975cdce8ac01fd7657a",
+    "url": "/portfolio/static/media/note2.da7c6d1a.png"
+  },
+  {
+    "revision": "3f722decbfa246148ca8f7012e4c6bfb",
+    "url": "/portfolio/static/media/pc-meelzer.3f722dec.png"
+  }
+]);
